b0aeac9fdb85f5a0645aa381b6087e8955b82e6e
John F <john@johnford.info>
2011-04-09 17:52:28 -0700

    add a script to make syncing mozilla dependencies easier

---
+++ deps/sync-mozilla.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script takes the root directory of the mozilla directory
+# and syncs the relevant locations into the spidernode tree.
+# Notes:
+#  1. this script doesn't touch the mozilla tree
+#  2. this script will add cruft in the mozilla tree
+
+set -ex
+
+test "x$1" != "x"
+test -d $1
+
+rsync -av $1/nsprpub mozjs/
+rsync -av $1/mfbt mozjs/
+rsync -av $1/js mozjs
+git add mozjs
+rev=`(cd $1 && git rev-parse HEAD)`
+git commit -m "syncing mozilla dependencies from revision $rev"
