1e9042a9dab13827ee73c376cf95a8b37286e58a
Till Schneidereit <tschneidereit@gmail.com>
2012-02-20 22:31:56 +0100

    Change sync-mozilla.sh to remove files removed in the source tree

--- deps/sync-mozilla.sh
+++ deps/sync-mozilla.sh
@@ -10,9 +10,10 @@ set -ex
 test "x$1" != "x"
 test -d $1
 
-rsync -av $1/nsprpub mozjs/
-rsync -av $1/mfbt mozjs/
-rsync -av $1/js mozjs
+rsync -av --delete $1/nsprpub mozjs/
+rsync -av --delete $1/mfbt mozjs/
+rsync -av --delete $1/js mozjs
 git add mozjs
+git rm -r `git ls-files --deleted mozjs`
 rev=`(cd $1 && git rev-parse HEAD)`
 git commit -m "syncing mozilla dependencies from revision $rev"
